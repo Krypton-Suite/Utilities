76e1593e09e12c49f3d0f9860b6e4280fc43ca3b
Peter Wagner <peterwagner@live.co.uk>
2020-02-26 12:04:00 +0000

    * Commit

---
+++ Source/Miscellaneous Toolkit Items/Templates/Visual Studio/CS/KryptonRibbonFormCS/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace KryptonRibbonFormCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.kryptonRibbon = new ComponentFactory.Krypton.Ribbon.KryptonRibbon();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.kryptonPanel = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.kryptonManager = new ComponentFactory.Krypton.Toolkit.KryptonManager(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonRibbon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonRibbon
+            // 
+            this.kryptonRibbon.AllowFormIntegrate = true;
+            this.kryptonRibbon.InDesignHelperMode = true;
+            this.kryptonRibbon.Name = "kryptonRibbon";
+            this.kryptonRibbon.SelectedTab = null;
+            this.kryptonRibbon.Size = new System.Drawing.Size(292, 142);
+            this.kryptonRibbon.TabIndex = 0;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.statusStrip.Location = new System.Drawing.Point(0, 243);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.ManagerRenderMode;
+            this.statusStrip.Size = new System.Drawing.Size(292, 22);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // kryptonPanel
+            // 
+            this.kryptonPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonPanel.Location = new System.Drawing.Point(0, 142);
+            this.kryptonPanel.Name = "kryptonPanel";
+            this.kryptonPanel.Size = new System.Drawing.Size(292, 101);
+            this.kryptonPanel.TabIndex = 2;
+            // 
+            // kryptonManager
+            // 
+            this.kryptonManager.GlobalPaletteMode = ComponentFactory.Krypton.Toolkit.PaletteModeManager.Office2010Blue;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 265);
+            this.Controls.Add(this.kryptonPanel);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.kryptonRibbon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonRibbon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbon kryptonRibbon;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel kryptonPanel;
+        private ComponentFactory.Krypton.Toolkit.KryptonManager kryptonManager;
+    }
+}
+
---
+++ Source/Miscellaneous Toolkit Items/Templates/Visual Studio/VB/KryptonRibbonFormVB/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits ComponentFactory.Krypton.Toolkit.KryptonForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.StatusStrip = New System.Windows.Forms.StatusStrip()
+        Me.KryptonRibbon = New ComponentFactory.Krypton.Ribbon.KryptonRibbon()
+        Me.KryptonPanel = New ComponentFactory.Krypton.Toolkit.KryptonPanel()
+        Me.KryptonManager = New ComponentFactory.Krypton.Toolkit.KryptonManager(Me.components)
+        CType(Me.KryptonRibbon, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.KryptonPanel, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'StatusStrip
+        '
+        Me.StatusStrip.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 243)
+        Me.StatusStrip.Name = "StatusStrip"
+        Me.StatusStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.ManagerRenderMode
+        Me.StatusStrip.Size = New System.Drawing.Size(292, 22)
+        Me.StatusStrip.TabIndex = 0
+        Me.StatusStrip.Text = "StatusStrip1"
+        '
+        'KryptonRibbon
+        '
+        Me.KryptonRibbon.AllowFormIntegrate = True
+        Me.KryptonRibbon.InDesignHelperMode = True
+        Me.KryptonRibbon.Name = "KryptonRibbon"
+        Me.KryptonRibbon.SelectedContext = Nothing
+        Me.KryptonRibbon.SelectedTab = Nothing
+        Me.KryptonRibbon.Size = New System.Drawing.Size(292, 142)
+        Me.KryptonRibbon.TabIndex = 1
+        '
+        'KryptonPanel
+        '
+        Me.KryptonPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.KryptonPanel.Location = New System.Drawing.Point(0, 142)
+        Me.KryptonPanel.Name = "KryptonPanel"
+        Me.KryptonPanel.Size = New System.Drawing.Size(292, 101)
+        Me.KryptonPanel.TabIndex = 2
+        '
+        'KryptonManager
+        '
+        Me.KryptonManager.GlobalPaletteMode = ComponentFactory.Krypton.Toolkit.PaletteModeManager.Office2010Blue
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 265)
+        Me.Controls.Add(Me.KryptonPanel)
+        Me.Controls.Add(Me.KryptonRibbon)
+        Me.Controls.Add(Me.StatusStrip)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.KryptonRibbon, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.KryptonPanel, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents StatusStrip As StatusStrip
+    Friend WithEvents KryptonRibbon As ComponentFactory.Krypton.Ribbon.KryptonRibbon
+    Friend WithEvents KryptonPanel As ComponentFactory.Krypton.Toolkit.KryptonPanel
+    Friend WithEvents KryptonManager As ComponentFactory.Krypton.Toolkit.KryptonManager
+End Class
